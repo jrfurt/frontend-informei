6686a1882d2e74cc169478b14b006b021e14d530
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-05-16 07:18:49 -0300

    feat: create Categorias page

---
+++ site-mei/src/pages/Categorias.jsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+// import DataTableClientes from '../components/DataTableClientes';
+
+export default function Clientes() {
+  return (
+    <div>
+      <Typography variant="h4" component="h1" mb={2} mx={1}>
+        Categorias
+      </Typography>
+      <DataTableClientes />
+    </div>
+  );
+}
