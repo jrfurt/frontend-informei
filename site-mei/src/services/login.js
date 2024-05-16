8f885487b75c2c5adcc5465edca111a871ab273e
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-05-16 11:52:26 -0300

    feat: add getNomeMeiById function to login.js

--- site-mei/src/services/login.js
+++ site-mei/src/services/login.js
@@ -4,6 +4,9 @@ const login = {
   logar: (form) => {
     return axiosInstance.post('/login', form);
   },
+  getNomeMeiById: (id_mei) => {
+    return axiosInstance.get('/login/getMeiId', { params: { id_mei } });
+  },
 };
 
 export default login;
