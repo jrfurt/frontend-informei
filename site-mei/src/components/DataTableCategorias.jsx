1b6829a7baf3fb0f3d0ee510096e6d186b0e6ad6
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-05-16 07:57:39 -0300

    feat: create DataTableCategorias.jsx

---
+++ site-mei/src/components/DataTableCategorias.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import servicosMei from '../services/servicosMei';
+
+const columns = [
+  { field: 'id_categoria', headerName: 'ID', width: 130 },
+  { field: 'categoria', headerName: 'Categoria', width: 250 },
+];
+
+export default function DataTable() {
+  const [rows, setRows] = useState(null);
+
+  useEffect(() => {
+    servicosMei.getAllCategorias().then((res) => {
+      setRows(res.data);
+    });
+  }, []);
+
+  if (!rows) return null;
+
+  return (
+    <>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          getRowId={(rows) => rows.id_categoria}
+          rows={rows}
+          columns={columns}
+          editMode="row"
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </div>
+    </>
+  );
+}
