020af34831ceabe1c736c7c59bb0d5a2b6d25f1e
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-05-16 07:18:07 -0300

    remove Dashboard route / add Categorias route

--- site-mei/src/App.jsx
+++ site-mei/src/App.jsx
@@ -6,16 +6,17 @@ import Clientes from './pages/Clientes';
 import Servicos from './pages/Servicos';
 import Login from './pages/Login';
 import CadastrarMei from './pages/CadastrarMei';
+import Categorias from './pages/Categorias'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SideBar />}>
-          <Route index element={<Dashboard />} />
-          <Route path="agendamentos" element={<Agendamentos />} />
+          <Route index element={<Agendamentos />} />
           <Route path="clientes" element={<Clientes />} />
           <Route path="servicos" element={<Servicos />} />
+          <Route path="categorias" element={<Categorias />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="cadastrar" element={<CadastrarMei />} />
